5956f63c5aa871d007e1ec2ae1c633585e2e00d3
AbtahiChowdhury <abtahichowdhury@gmail.com>
2020-04-04 12:12:09 -0400

    changes

--- Final/download_dataset.py
+++ Final/download_dataset.py
@@ -1,18 +1,28 @@
+import tarfile
 from tqdm import tqdm
 import requests
 
+def download(url):
+    buffer_size = 1024
+    r = requests.get(url,stream=True)
+
+    file_size = int(r.headers.get("Content-Length",0))
+    filename = url.split('/')[-1]
+
+    progress = tqdm(r.iter_content(buffer_size),f'Downloading {filename}',total=file_size,unit="B",unit_scale=True,unit_divisor=1024)
+    with open(filename,'wb') as f:
+        for data in progress:
+            f.write(data)
+            progress.update(len(data))
+
+def untar(filename):
+    tf = tarfile.open(filename)
+    tf.extractall()
+
 print('Beginning file download...')
-
 url = 'http://opihi.cs.uvic.ca/sound/genres.tar.gz'
+download(url)
 
-buffer_size = 1024
-r = requests.get(url,stream=True)
-
-file_size = int(r.headers.get("Content-Length",0))
-filename = url.split('/')[-1]
-
-progress = tqdm(r.iter_content(buffer_size),f'Downloading {filename}',total=file_size,unit="B",unit_scale=True,unit_divisor=1024)
-with open(filename,'wb') as f:
-    for data in progress:
-        f.write(data)
-        progress.update(len(data))
+print('Beginning file extraction...')
+filename = 'genres.tar.gz'
+untar(filename)
